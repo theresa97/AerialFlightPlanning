29039afac670ac1c2cd79e69f522d42e65d04d19
flightplanning <flightplanning@8c966314-88fc-47fc-8a40-bbc5bf1a50dc>
2014-10-27 13:02:31 +0000

    Add parse Framework and WeightandBalance Class

--- Aerial-Flightplaning/AppDelegate.m
+++ Aerial-Flightplaning/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [Parse setApplicationId:@"LvbHgOhydS56QecdmdtNwq4iEcPtczjfeuc0sj4C"
+                  clientKey:@"cccBLAo63ibXgJZxCssPp3kiWIFXE5QNjp0BLfs4"];
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     return YES;
 }
 
---
+++ Aerial-Flightplaning/WeightandBalance.m
@@ -0,0 +1,13 @@
+//
+//  WeightandBalance.m
+//  Aerial-Flightplaning
+//
+//  Created by Matthias Oberegger on 27.10.14.
+//  Copyright (c) 2014 Matthias Oberegger. All rights reserved.
+//
+
+#import "WeightandBalance.h"
+
+@implementation WeightandBalance
+
+@end
