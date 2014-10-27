fd8eb4259958544798461dae8fbe4f206bee4c6e
flightplanning <flightplanning@8c966314-88fc-47fc-8a40-bbc5bf1a50dc>
2014-10-27 13:02:31 +0000

    Add parse Framework and WeightandBalance Class

---
+++ branches/Projekt/Aerial-Flightplaning/WeightandBalance.h
@@ -0,0 +1,18 @@
+//
+//  WeightandBalance.h
+//  Aerial-Flightplaning
+//
+//  Created by Matthias Oberegger on 27.10.14.
+//  Copyright (c) 2014 Matthias Oberegger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WeightandBalance : NSObject
+
+@property NSInteger *BasicEmptyWeight;
+@property NSInteger *Arm;
+
+
+
+@end
