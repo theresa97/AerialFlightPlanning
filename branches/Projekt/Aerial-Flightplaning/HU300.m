7ec943ff20fc3161dd0e4612ede0d44d3de05620
flightplanning <flightplanning@8c966314-88fc-47fc-8a40-bbc5bf1a50dc>
2014-11-17 14:41:53 +0000

    Add class hu300 and init all variables next step create view to set basic weight...

--- branches/Projekt/Aerial-Flightplaning/HU300.m
+++ branches/Projekt/Aerial-Flightplaning/HU300.m
@@ -8,6 +8,60 @@
 
 #import "HU300.h"
 
-@implementation HU300
+@implementation HU300{
+
+}
+@synthesize BasicEmptyWeight;
+@synthesize BasicEmptyWeightArmLat;
+@synthesize BasicEmptyWeightArmLong;
+@synthesize PilotWeight;
+@synthesize PilotLongArm;
+@synthesize PilotLatArm;
+@synthesize COpilotweight;
+@synthesize COpilotLongArm;
+@synthesize COpilotLatArm;
+@synthesize MidPassangerWeight;
+@synthesize MidPassangerLongArm;
+@synthesize MidPassagerLatArm;
+@synthesize GloveBoxWeight;
+@synthesize GloveBoxLongArm;
+@synthesize GloveBoxLatArm;
+@synthesize MainFuelTankLatArm;
+@synthesize MainFuelTankLongArm;
+@synthesize MainFuelTankLiter;
+@synthesize AuxFuelTankLiter;
+@synthesize AuxFuelTankLatArm;
+@synthesize AuxFuelTankLongArm;
+
+
+-(void)initVariables:(NSDecimalNumber*)PilotWeighta and:(NSDecimalNumber*)COpilotWeighta and:(NSDecimalNumber*)MidPassangerWeighta and:(NSDecimalNumber*)MainTank and: (NSDecimalNumber*)AuxTank{
+    
+    BasicEmptyWeight = [[NSDecimalNumber alloc]init];
+    
+    BasicEmptyWeightArmLat = [[NSDecimalNumber alloc]initWithDouble:0.4];
+    BasicEmptyWeightArmLong = [[NSDecimalNumber alloc]initWithDouble:100.9];
+    PilotWeight = PilotWeighta;
+     PilotLongArm = [[NSDecimalNumber alloc]initWithDouble:83.2];
+     PilotLatArm = [[NSDecimalNumber alloc]initWithDouble:-13.8];
+    COpilotweight = COpilotWeighta;
+     COpilotLongArm = [[NSDecimalNumber alloc]initWithDouble:83.2];
+    COpilotLatArm = [[NSDecimalNumber alloc]initWithDouble:13.8];
+    MidPassangerWeight = [[NSDecimalNumber alloc]init];
+    MidPassangerLongArm = [[NSDecimalNumber alloc]initWithDouble:80];
+     MidPassagerLatArm = [[NSDecimalNumber alloc]initWithDouble:0.75];
+    GloveBoxWeight = [[NSDecimalNumber alloc]init];//wird noch benötig
+     GloveBoxLongArm = [[NSDecimalNumber alloc]init];//wird noch benötigt
+     GloveBoxLatArm = [[NSDecimalNumber alloc]init];
+    MainFuelTankLatArm = [[NSDecimalNumber alloc]initWithDouble:18];
+     MainFuelTankLongArm  = [[NSDecimalNumber alloc]initWithDouble:107];
+    MainFuelTankLiter = MainTank;
+    AuxFuelTankLiter = AuxTank;
+     AuxFuelTankLatArm = [[NSDecimalNumber alloc]initWithDouble:-17.2];
+     AuxFuelTankLongArm = [[NSDecimalNumber alloc]initWithDouble:107];
+    
+}
+
+
+
 
 @end
