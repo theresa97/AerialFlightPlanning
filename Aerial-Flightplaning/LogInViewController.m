1f5eaabe5868a29aa6e455d80d9b64012bd8ee07
flightplanning <flightplanning@8c966314-88fc-47fc-8a40-bbc5bf1a50dc>
2014-12-01 13:03:45 +0000

    Testen wieder möglich

--- Aerial-Flightplaning/LogInViewController.m
+++ Aerial-Flightplaning/LogInViewController.m
@@ -117,7 +117,7 @@
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    NSLog(@"hal");
+    //NSLog(@"hal");
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
