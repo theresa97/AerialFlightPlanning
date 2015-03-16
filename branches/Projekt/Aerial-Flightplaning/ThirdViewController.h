44802a19bb1cf1b63e1fadf35d6550cea4a48a07
tbuchegger <tbuchegger@8c966314-88fc-47fc-8a40-bbc5bf1a50dc>
2015-03-16 14:54:20 +0000

    Flugrechner GUI eingestellt

--- branches/Projekt/Aerial-Flightplaning/ThirdViewController.h
+++ branches/Projekt/Aerial-Flightplaning/ThirdViewController.h
@@ -21,6 +21,20 @@
 @property (weak, nonatomic) IBOutlet UIButton *BTNGewicht;
 @property (weak, nonatomic) IBOutlet UIPickerView *Picker301Outet;
 @property (weak, nonatomic) IBOutlet UIPickerView *Picker302Outlet;
+@property (weak, nonatomic) IBOutlet UIButton *Button1;
+@property (weak, nonatomic) IBOutlet UIButton *Button2;
+@property (weak, nonatomic) IBOutlet UIButton *Button3;
+@property (weak, nonatomic) IBOutlet UIButton *Button4;
+@property (weak, nonatomic) IBOutlet UIButton *Button5;
+@property (weak, nonatomic) IBOutlet UIButton *Button6;
+@property (weak, nonatomic) IBOutlet UIButton *Button7;
+@property (weak, nonatomic) IBOutlet UIButton *Button8;
+@property (weak, nonatomic) IBOutlet UIButton *Button9;
+@property (weak, nonatomic) IBOutlet UIButton *Button0;
+@property (weak, nonatomic) IBOutlet UIButton *ButtonAC;
+@property (weak, nonatomic) IBOutlet UIButton *ButtonMinus;
+@property (weak, nonatomic) IBOutlet UIButton *ButtonPlus;
+@property (weak, nonatomic) IBOutlet UIButton *ButtonIstgleich;
 
 
 - (IBAction)in301:(id)sender;
